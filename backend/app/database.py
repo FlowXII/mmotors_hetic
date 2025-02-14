371a0a1ad0680bad4d810ee196aeb32e8eae2d76
Steviggio <steve.mothmora@gmail.com>
2025-02-14 17:52:37 +0100

    fix: Testing docker services setup for API/DB connection

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,14 +6,14 @@ import os
 
 load_dotenv()
 
-POSTGRES_USER = os.getenv("POSTGRES_USER", "user")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "password")
-POSTGRES_SERVER = os.getenv("POSTGRES_SERVER", "postgres_db")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
-POSTGRES_DB = os.getenv("POSTGRES_DB", "dbname")
+POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "test")
+POSTGRES_SERVER = os.getenv("POSTGRES_SERVER", "postgres_db")  # FIXED
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")  # FIXED
+POSTGRES_DB = os.getenv("POSTGRES_DB", "mmotors")
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
-print(SQLALCHEMY_DATABASE_URL)
+print(f"Connecting to database: {SQLALCHEMY_DATABASE_URL}")  # Debug print
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -26,4 +26,4 @@ def get_db():
     try:
         yield db
     finally:
-        db.close() 
+        db.close()
